8615ac8ef9176fde7e9354a599364ffb6b93b3c2
98prabowo <58059518+98prabowo@users.noreply.github.com>
2024-11-21 21:27:51 +0700

    feat: implement queue on send and receive

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,37 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+};
 
 pub struct Sender<T> {
     inner: Arc<Inner<T>>,
+}
+
+impl<T> Sender<T> {
+    fn send(&mut self, value: T) {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.push_back(value);
+    }
 }
 
 pub struct Receiver<T> {
     inner: Arc<Inner<T>>,
 }
 
+impl<T> Receiver<T> {
+    fn recv(&mut self) -> Option<T> {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.pop_front()
+    }
+}
+
 struct Inner<T> {
-    queue: Mutex<Vec<T>>,
+    queue: Mutex<VecDeque<T>>,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: Mutex::new(Vec::default()),
+        queue: Mutex::new(VecDeque::default()),
     };
     let inner = Arc::new(inner);
     (
